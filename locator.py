0cb59af8fbb1cce323a61c7b3d39c98da529b8ae
Vlada Vasileva <vvavlada@gmail.com>
2023-05-30 13:31:33 +0300

    Sprint_3.2

--- locator.py
+++ locator.py
@@ -2,12 +2,19 @@ from selenium.webdriver.common.by import By
 
 class Locators:
     ACC_BUTTON = (By.XPATH, '//p[@class="AppHeader_header__linkText__3q_va ml-2"]') #Личный кабинет
-    REG_BUTTON = (By.XPATH, '//button[@class="button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa"]') #Кнопка регистрации
-    ENT_BUTTON = (By.XPATH, '//button[@class="button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa"]') #Кнопка Войти
+    REG_BUTTON = (By.XPATH, '//a[@class="Auth_link__1fOlj"]') #Кнопка Зарегистрироваться
+    ENT_BUTTON = (By.ID, 'Войти') #Кнопка Войти
+    REGISTRATION_BUTTON = (By.ID, 'Зарегистрироваться') #Кнопка-огурец Зарегистрироваться
+    RECOVER_BUTTON = (By.ID, 'Восстановить') #Кнопка Восстановить
+    SAVE_BUTTON = (By.ID, 'Сохранить') #Кнопка Сохранить
     INCORECT_PASSWORD_TEXT = (By.XPATH,'//p[@class="input__error text_type_main-default"]') #Текст - "Некорректный пароль"
-    RESERT_BUTTON = (By.XPATH, '//button[@class="button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa"]') #Кнопка Восстановить
-    SAVE_BUTTON = (By.XPATH, '//button[@class="button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa"]') #Кнопка Сохранить
-    CONSTRUCTOR_BUTTON = (By.XPATH, '//p[@class="AppHeader_header__linkText__3q_va ml-2"]') #Кнопка Конструктор
+    CONSTRUCTOR_BUTTON = (By.ID, 'Конструктор') #Кнопка Конструктор
     LOGO_BUTTON =  (By.XPATH, '//div[@class="AppHeader_header__logo__2D0X2"]') #логотип Stellar Burgers
     EXIT_BUTTON = (By.XPATH, '//button[@class="Account_button__14Yp3 text text_type_main-medium text_color_inactive"]') #Кнопка Выход
-    SECTIONS = (By.XPATH, '//div[@class="tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect"]') #Секция Булки
+    SAUSE_SECTIONS = (By.ID, 'Соусы') #Секция Соусы
+    BUNS_SECTIONS = (By.ID, 'Булки')  #Секция Булки
+    TOPPING_SECTIONS = (By.ID, 'Начинки')  # Секция Начинки
+    EMAIL = (By.ID, 'Email') # email
+    PASSWORD = (By.ID, 'Пароль') # password
+    NAME = (By.NAME, 'name') #name
+    CODE_RECOVER = (By.ID, 'Введите код из письма') #Поле Восстановить пароль
--- test.py
+++ test.py
@@ -1,104 +1,110 @@
-from selenium.webdriver.common.by import By
+import email
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from locator import Locators
 
-faker = Faker()
-class TestStellarBurgers(unittest.TestCase):
+class TestStellarBurgersRegistration():
     def test_registration_success(self, driver):
-        driver.find_element(By.XPATH, '//p[@class="AppHeader_header__linkText__3q_va ml-2"]').click()
-        driver.find_element(By.XPATH, '//a[@class="Auth_link__1fOlj"]').click()
-        driver.find_element(By.NAME, 'name').send_keys('Влада')
-        driver.find_element(By.ID, 'Email').send_keys(email)
-        driver.find_element(By.ID, 'Пароль').send_keys('123456789')
-        driver.find_element(By.XPATH, '//button[@class="button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa"]').click()
-        driver.find_element(By.ID, 'Email').send_keys(email)
-        driver.find_element(By.ID, 'Пароль').send_keys('123456789')
-        driver.find_element(By.XPATH, '//button[@class="button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa"]').click()
+        driver.find_element(*Locators.ACC_BUTTON).click()
+        driver.find_element(*Locators.REG_BUTTON).click()
+        driver.find_element(*Locators.NAME).send_keys('Влада')
+        driver.find_element(*Locators.EMAIL).send_keys(email)
+        driver.find_element(*Locators.PASSWORD).send_keys('123456789')
+        driver.find_element(*Locators.REGISTRATION_BUTTON).click()
+        driver.find_element(*Locators.EMAIL).send_keys(email)
+        driver.find_element(*Locators.PASSWORD).send_keys('123456789')
+        driver.find_element(*Locators.ENT_BUTTON).click()
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site'
-        driver.quite()
 
     def test_registration_incorrect_password(self, driver):
-        driver.find_element(By.XPATH, '//p[@class="AppHeader_header__linkText__3q_va ml-2"]').click()
-        driver.find_element(By.ID, 'Email').send_keys(email)
-        driver.find_element(By.ID, 'Пароль').send_keys('1234')
-        driver.find_element(By.XPATH, '//button[@class="button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa"]').click()
-        driver.find_element(By.XPATH,'//p[@class="input__error text_type_main-default"]' )
-        reg_text = WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.XPATH, '//p[@class="input__error text_type_main-default"]'))).text
+        driver.find_element(*Locators.ACC_BUTTON).click()
+        driver.find_element(*Locators.EMAIL).send_keys(email)
+        driver.find_element(*Locators.PASSWORD).send_keys('1234')
+        driver.find_element(*Locators.ENT_BUTTON).click()
+        driver.find_element(*Locators.INCORECT_PASSWORD_TEXT)
+        reg_text = WebDriverWait(driver, 3).until(EC.visibility_of_element_located(*Locators.INCORECT_PASSWORD_TEXT)).text
         assert reg_text
         driver.quite()
 
+class TestStellarBurgersLogin():
     def test_login_button(selfself, driver):
         driver.get('https://stellarburgers.nomoreparties.site/login')
-        driver.find_element(By.ID, 'Email').send_keys(email)
-        driver.find_element(By.ID, 'Пароль').send_keys('123456789')
-        driver.find_element(By.XPATH, '//button[@class="button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa"]').click()
+        driver.find_element(*Locators.EMAIL).send_keys(email)
+        driver.find_element(*Locators.PASSWORD).send_keys('123456789')
+        driver.find_element(*Locators.ENT_BUTTON).click()
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site'
-        driver.quite()
 
     def test_login_with_account_button(selfself, driver):
-        driver.find_element(By.XPATH, '//p[@class="AppHeader_header__linkText__3q_va ml-2"]').click()
-        driver.find_element(By.ID, 'Email').send_keys(email)
-        driver.find_element(By.ID, 'Пароль').send_keys('123456789')
-        driver.find_element(By.XPATH, '//button[@class="button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa"]').click()
+        driver.find_element(*Locators.ACC_BUTTON).click()
+        driver.find_element(*Locators.EMAIL).send_keys(email)
+        driver.find_element(*Locators.PASSWORD).send_keys('123456789')
+        driver.find_element(*Locators.ENT_BUTTON).click()
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site'
-        driver.quite()
 
     def test_login_with_registertion_button(self, driver):
         driver.get('https://stellarburgers.nomoreparties.site/register')
-        driver.find_element(By.NAME, 'name').send_keys('Влада')
-        driver.find_element(By.ID, 'Email').send_keys(email)
-        driver.find_element(By.ID, 'Пароль').send_keys('123456789')
-        driver.find_element(By.XPATH, '//button[@class="button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa"]').click()
+        driver.find_element(*Locators.NAME).send_keys('Влада')
+        driver.find_element(*Locators.EMAIL).send_keys(email)
+        driver.find_element(*Locators.PASSWORD).send_keys('123456789')
+        driver.find_element(*Locators.REGISTRATION_BUTTON).click()
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site'
-        driver.quite()
 
     def test_login_with_forgot_password_button(self, driver):
         driver.get('https://stellarburgers.nomoreparties.site/forgot-password')
-        driver.find_element(By.ID, 'Email').send_keys(email)
-        driver.find_element(By.XPATH, '//button[@class="button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa"]').click()
+        driver.find_element(*Locators.NAME).send_keys(email)
+        driver.find_element(*Locators.RECOVER_BUTTON).click()
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site/reset-password'
-        driver.find_element(By.ID, 'Пароль').send_keys('12345678')
-        driver.find_element(By.ID, 'Введите код из письма').send_keys('12345')
-        driver.find_element(By.XPATH, '//button[@class="button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa"]').click()
+        driver.find_element(*Locators.PASSWORD).send_keys('123456789')
+        driver.find_element(*Locators.CODE_RECOVER).send_keys('12345')
+        driver.find_element(*Locators.SAVE_BUTTON).click()
         WebDriverWait(driver, 3)
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site'
         driver.quite()
 
+class TestStellarBurgersAccount():
     def test_account_tap(self, driver):
         driver.get('https://stellarburgers.nomoreparties.site/')
-        driver.find_element(By.XPATH, '//p[@class="AppHeader_header__linkText__3q_va ml-2"]').click()
+        driver.find_element(*Locators.ACC_BUTTON).click()
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site/account/profile'
         driver.quite()
 
+class TestStellarBurgersConstructor():
     def test_constructor_button_tap(self, driver):
         driver.get('https://stellarburgers.nomoreparties.site/account/profile')
-        driver.find_element(By.XPATH, '//p[@class="AppHeader_header__linkText__3q_va ml-2"]').click()
+        driver.find_element(*Locators.CONSTRUCTOR_BUTTON).click()
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site'
-        driver.quite()
 
     def test_logo_tap(self, driver):
         driver.get('https://stellarburgers.nomoreparties.site/account/profile')
-        driver.find_element(By.XPATH, '//div[@class="AppHeader_header__logo__2D0X2"]').click()
+        driver.find_element(*Locators.LOGO_BUTTON).click()
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site'
         driver.quite()
 
+class TestStellarBurgersExit():
     def test_exit_button_tap(self, driver):
         driver.get('https://stellarburgers.nomoreparties.site/account/profile')
-        driver.find_element(By.XPATH, '//button[@class="Account_button__14Yp3 text text_type_main-medium text_color_inactive"]').click()
+        driver.find_element(*Locators.EXIT_BUTTON).click()
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site/login'
         driver.quite()
 
-    def test_change_section(self, driver):
+class TestStellarBurgersSections():
+    def test_change_section_sause(self, driver):
         driver.get('https://stellarburgers.nomoreparties.site/')
-        driver.find_element(By.XPATH, '//div[@class="tab_tab__1SPyG  pt-4 pr-10 pb-4 pl-10 noselect"]').click
-        section_name = driver.find_element(By.XPATH, '//h2[@class="text text_type_main-medium mb-6 mt-10"]').text()
+        driver.find_element(*Locators.SAUSE_SECTIONS).click
+        section_name = driver.find_element(*Locators.SAUSE_SECTIONS).text()
         assert section_name == 'Соусы'
-        driver.find_element(By.XPATH, '//div[@class="tab_tab__1SPyG  pt-4 pr-10 pb-4 pl-10 noselect"]').click()
-        section_name = driver.find_element(By.XPATH, '//h2[@class="text text_type_main-medium mb-6 mt-10"]').text()
+
+    def test_change_section_buns(self, driver):
+        driver.find_element(*Locators.BUNS_SECTIONS).click()
+        section_name = driver.find_element(*Locators.BUNS_SECTIONS).text()
         assert section_name == 'Булки'
-        driver.find_element(By.XPATH, '//div[@class="tab_tab__1SPyG  pt-4 pr-10 pb-4 pl-10 noselect"]').click
-        section_name = driver.find_element(By.XPATH, '//h2[@class="text text_type_main-medium mb-6 mt-10"]').text()
+
+    def test_change_section_topping(self, driver):
+        driver.find_element(*Locators.TOPPING_SECTIONS).click
+        section_name = driver.find_element(*Locators.TOPPING_SECTIONS).text()
         assert section_name == 'Начинки'
         driver.quite()
+
 
 
 
