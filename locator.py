1403a7a2eb42c5b9bfe1e0b11b67fd85ae975dac
Vlada Vasileva <vvavlada@gmail.com>
2023-05-30 18:10:48 +0300

    Sprint_3.3

--- locator.py
+++ locator.py
@@ -1,23 +1,43 @@
 from selenium.webdriver.common.by import By
 
 class Locators:
-    ACC_BUTTON = (By.XPATH, '//p[@class="AppHeader_header__linkText__3q_va ml-2"]') #Личный кабинет
-    REG_BUTTON = (By.XPATH, '//a[@class="Auth_link__1fOlj"]') #Кнопка Зарегистрироваться
-    ENT_BUTTON = (By.XPATH, '//button[contains(text(),"Войти")]') #Кнопка Войти
-    REGISTRATION_BUTTON = (By.XPATH, '//button[contains(text(),"Зарегистрироваться")']) #Кнопка-огурец Зарегистрироваться
-    RECOVER_BUTTON = (By.XPATH, '//button[contains(text(),"Восстановить")]') #Кнопка Восстановить
-    SAVE_BUTTON = (By.XPATH, '//button[contains(text(),"Сохранить")]') #Кнопка Сохранить
-    INCORECT_PASSWORD_TEXT = (By.XPATH,'//p[@class="input__error text_type_main-default"]') #Текст - "Некорректный пароль"
-    CONSTRUCTOR_BUTTON = (By.XPATH, '//p[contains(text(),"Конструктор")]') #Кнопка Конструктор
-    LOGO_BUTTON =  (By.XPATH, '//div[@class="AppHeader_header__logo__2D0X2"]') #логотип Stellar Burgers
-    EXIT_BUTTON = (By.XPATH, '//button[@class="Account_button__14Yp3 text text_type_main-medium text_color_inactive"]') #Кнопка Выход
-    SAUSE_SECTIONS = (By.XPATH, '//span[contains(text(),"Соусы")]') #Секция Соусы
-    BUNS_SECTIONS = (By.XPATH, '//span[contains(text(),"Булки")]')  #Секция Булки
-    TOPPING_SECTIONS = (By.XPATH, '//span[contains(text(),"Начинки")]')  # Секция Начинки
-    BUNS_HEADING = (By.XPATH, "//h2[contains(text(),'Булки')]") #Заголовок Булки
-    SAUSE_HEADING = (By.XPATH, "//h2[contains(text(),'Соусы')]") #Заголовок Соусы
-    TOPPING_HEADING = (By.XPATH, "//h2[contains(text(),'Начинки)]") #Заголовок Начинки
-    EMAIL = (By.ID, 'Email') # email
-    PASSWORD = (By.ID, 'Пароль') # password
-    NAME = (By.NAME, 'name') #name
-    CODE_RECOVER = (By.ID, 'Введите код из письма') #Поле Восстановить пароль
+    # Личный кабинет
+    ACC_BUTTON = (By.XPATH, '//p[@class="AppHeader_header__linkText__3q_va ml-2"]')
+    # Кнопка Зарегистрироваться
+    REG_BUTTON = (By.XPATH, '//a[@class="Auth_link__1fOlj"]')
+    # Кнопка Войти
+    ENT_BUTTON = (By.XPATH, '//button[contains(text(),"Войти")]')
+    # Кнопка-огурец Зарегистрироваться
+    REGISTRATION_BUTTON = (By.XPATH, '//button[contains(text(),"Зарегистрироваться")'])
+    # Кнопка Восстановить
+    RECOVER_BUTTON = (By.XPATH, '//button[contains(text(),"Восстановить")]')
+    # Кнопка Сохранить
+    SAVE_BUTTON = (By.XPATH, '//button[contains(text(),"Сохранить")]')
+    # Текст - "Некорректный пароль"
+    INCORECT_PASSWORD_TEXT = (By.XPATH,'//p[@class="input__error text_type_main-default"]')
+    # Кнопка Конструктор
+    CONSTRUCTOR_BUTTON = (By.XPATH, '//p[contains(text(),"Конструктор")]')
+    # логотип Stellar Burgers
+    LOGO_BUTTON =  (By.XPATH, '//div[@class="AppHeader_header__logo__2D0X2"]')
+    # Кнопка Выход
+    EXIT_BUTTON = (By.XPATH, '//button[@class="Account_button__14Yp3 text text_type_main-medium text_color_inactive"]')
+    # Секция Соусы
+    SAUSE_SECTIONS = (By.XPATH, '//span[contains(text(),"Соусы")]')
+    # Секция Булки
+    BUNS_SECTIONS = (By.XPATH, '//span[contains(text(),"Булки")]')
+    # Секция Начинки
+    TOPPING_SECTIONS = (By.XPATH, '//span[contains(text(),"Начинки")]')
+    # Заголовок Булки
+    BUNS_HEADING = (By.XPATH, "//h2[contains(text(),'Булки')]")
+    # Заголовок Соусы
+    SAUSE_HEADING = (By.XPATH, "//h2[contains(text(),'Соусы')]")
+    # Заголовок Начинки
+    TOPPING_HEADING = (By.XPATH, "//h2[contains(text(),'Начинки')]")
+    # email
+    EMAIL = (By.ID, 'Email')
+    # password
+    PASSWORD = (By.ID, 'Пароль')
+    # name
+    NAME = (By.NAME, 'name')
+    # Поле Восстановить пароль
+    CODE_RECOVER = (By.XPATH, "//label[contains(text(),'Введите код из письма')]")
